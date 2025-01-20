3d308071d21944364965cd75e73f0a2899bdaab0
小叶 <1936472877@qq.com>
2025-01-20 14:43:24 +0800

    perf: 优化插件加载速度

--- apps/index.js
+++ apps/index.js
@@ -7,21 +7,25 @@ const startTime = Date.now()
 
 const path = join(Version.pluginPath, 'apps')
 
-const files = fs.readdirSync(path).filter(file => file.endsWith('.js'))
-
 const apps = {}
-for (const i of files) {
-  if (i === 'index.js') continue
+
+fs.readdirSync(path).forEach(file => {
+  if (file.endsWith('.js') && file !== 'index.js') {
+    apps[file.replace('.js', '')] = import(`file://${join(path, file)}`)
+  }
+})
+
+await Promise.all(Object.keys(apps).map(async id => {
   try {
     const startTime = Date.now()
-    const exp = await import(`file://${join(path, i)}`)
-    const id = i.replace('.js', '')
+    const exp = await apps[id]
     apps[id] = exp.app
-    logger.debug(`加载js: apps/${i}成功 耗时: ${Date.now() - startTime}ms`)
+    logger.debug(`加载js: apps/${id}.js成功 耗时: ${Date.now() - startTime}ms`)
   } catch (error) {
-    logger.error('error', `加载js: apps/${i}错误\n`, error)
+    delete apps[id]
+    logger.log('error', `加载js: apps/${id}.js错误\n`, error)
   }
-}
+}))
 
 export { apps }
 
